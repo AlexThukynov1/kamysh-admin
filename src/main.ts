8c8d187430a5d179a3157405b71660bb31f60f39
Tsykunov Oleksandr <50213824+AlexThukynov1@users.noreply.github.com>
2025-04-22 12:00:39 +0300

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/index'
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persist';
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+createApp(App).use(router).use(pinia).mount('#app')
---
+++ src/store/index.ts
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia';
+
+export const useCounterStore = defineStore('counter', {
+  state: () => ({
+    count: 0,
+    name: 'Мій лічильник',
+  }),
+  getters: {
+    doubleCount: (state) => state.count * 2,
+  },
+  actions: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+    changeName(newName: string) {
+      this.name = newName;
+    },
+  },
+  persist: true,
+});
